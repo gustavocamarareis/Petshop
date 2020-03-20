c3f2cc7f0b98779d4664d470cc251bebdbc65de3
hockpond <gustavoc.reis@hotmail.com>
2020-03-20 18:40:28 -0300

    Conecta o banco de dados ao servidor

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://PETshop:saladadefruta@petshop-7bazx.mongodb.net/test?retryWrites=true&w=majority')
 
 const servidor = express()
 
