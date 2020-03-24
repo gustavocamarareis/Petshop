e7e7d633be31cb91a84099e386a9596f21250348
Laura <laurazsouza@yahoo.com.br>
2020-03-24 18:32:39 -0300

    adiciona rota de cadastro no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const roteador = require('./routes')
 
 mongoose.connect('mongodb+srv://PETshop:saladadefruta@petshop-7bazx.mongodb.net/test?retryWrites=true&w=majority')
 
 const servidor = express()
+
+servidor.use(express.json())
+
+servidor.use(roteador)
 
 servidor.listen(1234)
