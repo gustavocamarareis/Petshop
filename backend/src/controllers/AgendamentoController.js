5d275d5d5569178fc44b670873a0a9632d2e8aff
NIKIBIN <NIKIBIN@users.noreply.github.com>
2020-04-01 18:13:27 -0300

    adiciona rotas de edicição de agendamento

--- backend/src/controllers/AgendamentoController.js
+++ backend/src/controllers/AgendamentoController.js
@@ -7,5 +7,14 @@ module.exports = {
         const agendamento = await Agendamento.create ({ dia, mes, ano, hora, minuto, nomeCliente, nomeCachorro, obs, telefone })
         
         return res.json (agendamento)
+    },
+    async edit(req, res){    
+        const { agendamento_id } = req.query
+        const { dia, mes, ano, hora, minuto, nomeCliente, nomeCachorro, obs, telefone } = req.body
+    
+        await Agendamento.updateOne ({ _id: agendamento_id}, { dia, mes, ano, hora, minuto, nomeCliente, nomeCachorro, obs, telefone })
+        let agendamento = await Agendamento.findOne ({ _id: agendamento_id })
+
+        return res.json (agendamento)
     }
 }
