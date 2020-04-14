6c8b984cde5866469be888b406d070d6741b3b95
Gustavo Câmara Reis <gustavoc.reis@hotmail.com>
2020-04-14 18:01:25 -0300

    teste

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const roteador = require('./routes')
-
-mongoose.connect('mongodb+srv://PETshop:saladadefruta@petshop-7bazx.mongodb.net/test?retryWrites=true&w=majority')
+const cors = require('cors')
 
 const servidor = express()
 
+mongoose.connect('mongodb+srv://PETshop:saladadefruta@petshop-7bazx.mongodb.net/test?retryWrites=true&w=majority')
+
+servidor.use(cors())
 servidor.use(express.json())
-
 servidor.use(roteador)
-
 servidor.listen(1234)
